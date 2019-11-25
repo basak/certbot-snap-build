4cf2fd35a0e9feaaea4f0f263248e5fd6c22153a
Adrien Ferrand <ferrand.ad@gmail.com>
2019-11-25 17:44:58 +0100

    Duplicated sudo

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-sudo snap install --dangerous --classic *.snap
+snap install --dangerous --classic *.snap
 
 python3 -m venv venv
 venv/bin/python -m pip install -e certbot/certbot-ci
