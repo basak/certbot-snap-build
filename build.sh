8237c2c7a54955786f856775fce6bca013e77603
Adrien Ferrand <ferrand.ad@gmail.com>
2019-11-26 16:20:08 +0100

    Update before snapcraft

--- build.sh
+++ build.sh
@@ -14,4 +14,4 @@ function cleanup() {
 trap cleanup EXIT
 
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker run --rm --net=host -v $(pwd):$(pwd) -w $(pwd) -e "PIP_EXTRA_INDEX_URL=http://localhost:8080/simple/" -t "adferrand/snapcraft:${SNAP_ARCH}" snapcraft --destructive-mode
+docker run --rm --net=host -v $(pwd):$(pwd) -w $(pwd) -e "PIP_EXTRA_INDEX_URL=http://localhost:8080/simple/" -t "adferrand/snapcraft:${SNAP_ARCH}" bash -c "apt-get update && snapcraft"
