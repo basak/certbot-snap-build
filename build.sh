0613a7c8a24afb609267d6f85d9f54c1668ecced
Adrien Ferrand <ferrand.ad@gmail.com>
2020-02-23 15:47:32 +0100

    Update to use snapcraft 3.10

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -em
 
 SNAP_ARCH=$1
 
-docker run --net=host --rm -v "$(pwd)/packages:/data/packages" --name pypiserver pypiserver/pypiserver -d
+docker run --net=host -d --rm -v "$(pwd)/packages:/data/packages" --name pypiserver pypiserver/pypiserver
 
 PIP_SERVER_PID=$!
 
@@ -14,4 +14,4 @@ function cleanup() {
 trap cleanup EXIT
 
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker run --rm --net=host -v $(pwd):$(pwd) -w $(pwd) -e "PIP_EXTRA_INDEX_URL=http://localhost:8080/simple/" -t "adferrand/snapcraft:${SNAP_ARCH}" bash -c "apt-get update && snapcraft"
+docker run --rm --net=host -v $(pwd):$(pwd) -w $(pwd) -e "PIP_EXTRA_INDEX_URL=http://localhost:8080/simple" -t "builder:${SNAP_ARCH}" bash -c "apt-get update && snapcraft"
